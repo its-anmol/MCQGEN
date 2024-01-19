57b8b93c727752b4f96e7e92bf245d872d75c0da
its-anmol <anmol70806@gmail.com>
2024-01-19 05:57:53 +0530

    jupyter implementation complete

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import  find_packages,setup
+
+setup(
+    name='mcqgenerator',
+    version='0.0.1',
+    author='anmol70806@gmail.com',
+    install_requires=[],
+    packages=find_packages()
+)
